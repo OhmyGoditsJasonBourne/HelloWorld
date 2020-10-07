4fead29e43628c98c48018f1727831d1a1257289
OhmyGoditsJasonBourne <72467379+OhmyGoditsJasonBourne@users.noreply.github.com>
2020-10-07 19:25:49 +0200

    Update HelloWorld.java

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -2,7 +2,7 @@ package HelloWorld;
 
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Oh my god it's not Hello World");
+        System.out.println("Oh my god it's not Hello World!");
         try{Thread.sleep(1000);}
         catch(InterruptedException ex){};
         System.out.println("IT'S JASON BOURNE!");
