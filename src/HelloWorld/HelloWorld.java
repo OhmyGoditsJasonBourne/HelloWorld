04b9623de089d8abe18b08000e99aea0e924a4ec
OhmyGoditsJasonBourne <72467379+OhmyGoditsJasonBourne@users.noreply.github.com>
2020-10-07 19:18:30 +0200

    First Commit

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
         System.out.println("Oh my god it's not Hello World");
         try{Thread.sleep(1000);}
         catch(InterruptedException ex){};
-        System.out.println("IT'S JASON BOURNE");
+        System.out.println("IT'S JASON BOURNE!");
 
 
     }
